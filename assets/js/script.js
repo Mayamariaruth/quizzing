586dcfc703ac1c7795acb30216b7ee41635ed14d
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2023-12-18 16:43:18 +0800

    feat:add styling to quiz box and add rest of questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,65 @@
+/* Credit: Codehal YT video: https://www.youtube.com/watch?v=Vp8x8-reqZA&ab_channel=Codehal */
+Let questions = [ {
+    nr: 1,
+    question: 'Who is known as the "King of Pop"?',
+    answer: "Michael Jackson",
+    options: [
+        "Chris Brown",
+        "Elvis Presley",
+        "Sean Paul",
+        "Michael Jackson"
+    ]
+},
+{
+    nr: 2,
+    question: 'What is the largest planet in our solar system?',
+    answer: "Jupiter",
+    options: [
+        "Pluto",
+        "Jupiter",
+        "Uranus",
+        "Earth"
+    ]
+},
+{
+    nr: 3,
+    question: 'What is the capital city of Australia?',
+    answer: "Canberra",
+    options: [
+        "Canberra",
+        "Sydney",
+        "Melbourne",
+        "Brisbane"
+    ]
+},
+{
+    nr: 4,
+    question: 'What is the currency of Japan?',
+    answer: "Yen",
+    options: [
+        "Rupiah",
+        "Yuan",
+        "Yen",
+        "Won"
+    ]
+},
+{
+    nr: 5,
+    question: 'Who was the first President of the United States?',
+    answer: "George Washington",
+    options: [
+        "Thomas Jefferson",
+        "Jimmy Carter",
+        "Abraham Lincoln",
+        "George Washington"
+    ]
+}
+]
+
+function showQuestions(index) {
+    
+}
+
 /* Credit: CI Love Maths project */
 function checkAnswer() {
 
@@ -13,20 +75,4 @@ function wrongAnswer() {
 
 function incrementCorrectScore() {
 
-}
-
-function secondQuestion() {
-
-}
-
-function thirdQuestion() {
-    
-}
-
-function fourthQuestion() {
-    
-}
-
-function fifthQuestion() {
-    
 }
