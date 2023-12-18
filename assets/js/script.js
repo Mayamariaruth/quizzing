010b4afdd02ae571210ee28f94ba8c37185f3595
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2023-12-18 14:26:21 +0800

    feat:center quiz box and add first question/answer and function structure in js

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/* Credit: CI Love Maths project */
+function checkAnswer() {
+
+}
+
+function correctAnswer() {
+
+}
+
+function wrongAnswer() {
+
+}
+
+function incrementCorrectScore() {
+
+}
+
+function secondQuestion() {
+
+}
+
+function thirdQuestion() {
+    
+}
+
+function fourthQuestion() {
+    
+}
+
+function fifthQuestion() {
+    
+}
